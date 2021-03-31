51ecc8ec2524e9e6697a407aebde2d0dea4a4aa7
namvkim <sonnamttt@gmail.com>
2021-03-31 10:33:00 +0700

    data complete

--- data.sql
+++ data.sql
@@ -7,30 +7,21 @@ create table Users(
   address varchar(50)
 );
 insert into Users(name_user, pass, phone_num, email, address)
-values('Nam', '123', 12343, 'jhsdfg', 'Da Nang'),
-('Ni', '234', 23435255, 'ni@gmal.com', 'Da Nang');
+values('Nam', '123', 12343, 'sonnamttt@gmail.com', 'Da Nang'),
+  ('Ni', '234', 23435255, 'ni@gmal.com', 'Da Nang');
 create table admin(user_AD varchar(50), pass varchar(15));
-insert into admin(user_AD, pass) value('Sang', '12345');
+insert into admin(user_AD, pass) value('admin', '12345');
 create table Products(
   ID_pro int primary key auto_increment,
   name_pro varchar(50),
   price decimal(10, 2),
-  describes varchar(100)
+  describes varchar(100),
+  category char(50)
 );
-insert into Products(name_pro, price, describes)
-values('Bánh', '10000', 'Ngọt'),
-('Kẹo', '20000', 'Chua');
-create table orders(
-  ID_order int primary key auto_increment,
-  ID_user int,
-  ID_pro int,
-  datee varchar(50),
-  FOREIGN KEY (ID_user) REFERENCES Users (ID_user),
-  FOREIGN KEY (ID_pro) REFERENCES Products (ID_pro)
-);
-insert into orders(ID_user, ID_pro, datee)
-values(1, 1, '25-3-2005'),
-(1, 2, '15-3-2005');
+insert into Products(name_pro, price, describes, category)
+values('Bánh', '10000', 'Ngọt', 1),
+  ('Kẹo', '20000', 'Chua', 1),
+  ('Coca cola', '50000', 'ngon', 2);
 create table picture(
   ID_pro int,
   FOREIGN KEY (ID_pro) REFERENCES Products (ID_pro),
@@ -45,6 +36,38 @@ values(
     1,
     'https://bizweb.dktcdn.net/thumb/large/100/004/714/products/keo-marshies-trai-tim-vi-dau-1.png?v=1605501325813'
   );
+create table orders(
+  ID_order int primary key auto_increment,
+  ID_user int,
+  FOREIGN KEY (ID_user) REFERENCES Users (ID_user)
+);
+insert into orders(ID_user)
+values(1), (2);
+
+create table order_details(
+  ID_order int,
+  ID_pro int,
+  quantity int,
+  datee varchar(50),
+  FOREIGN KEY (ID_order) REFERENCES orders (ID_order),
+  FOREIGN KEY (ID_pro) REFERENCES Products (ID_pro)
+);
+
+insert into order_details(ID_order, ID_pro, quantity, datee)
+values(1, 1, 15, '25-3-2005'), 
+  (1, 2, 20, '15-3-2005');
+
+create table cart(
+  ID_cart int primary key auto_increment,
+  ID_user int,
+  ID_pro int,
+  quantity int,
+  FOREIGN KEY (ID_user) REFERENCES Users (ID_user),
+  FOREIGN KEY (ID_pro) REFERENCES Products (ID_pro)
+);
+insert into cart(ID_user, ID_pro, quantity)
+values(1, 1, 15),
+  (1, 2, 20);
 create table material(
   ID_mate int primary key auto_increment,
   name_mate varchar(30),
@@ -53,5 +76,5 @@ create table material(
   date_input varchar(50)
 );
 insert into material(name_mate, amount_mate, price_mate, date_input)
-values('Nam', 1, 10000, '11-12-2019'),
-('Ni', 2, 10000, '10-11-2017');
+values('Cá', 1, 10000, '11-12-2019'),
+  ('Thịt', 2, 10000, '10-11-2017');
