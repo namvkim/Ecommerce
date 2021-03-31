1b3c1046b898626e8fed0a0683a7287ccc120b3b
Ho Thi Da Ni <ni.ho22@pn.com.vn>
2021-03-31 08:40:51 +0700

    Giao dien

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,19 +18,88 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="./resources/css/login.css">
+    <style>
+  
+
+    </style>
 </head>
 
 <body>
-    <div class="header">
-       
+    <div class="header" style="background-color :cadetblue; height:80px; padding-top:15px;">
+    
+        <h2 >ĐĂNG NHẬP</h2>
     </div>
-    <div class="content">
+    <div class="content" style="background-image: url('./resources/img/foodd.jpg');
+      background-repeat:no-repeat; background-size:100%; height:615px; width:100% ;  display: flex; align-items: center; " >
+        
+        
+        <div class="container content-input" >
+            <form action="/action_page.php">
+                <br>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="name" placeholder="Nhập tên đăng nhập" name="name">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="password" placeholder="Nhập mật khẩu" name="password">
+                </div>
 
+
+                <DIV class=" content-input-flex" >
+                    <div >
+                        <input type="checkbox" id="checkbox1" name="checkbox1" >
+                        <label for="checkbox1"> Lưu đăng nhập</label><br>
+
+                    </div>
+                    <div >
+                        <a><u> Quên mật khẩu</u> </a>
+                    </div>
+                </DIV>
+                <!-- <a href=""><input type="submit" name="btn" value="Sign In"></input></a> -->
+                <a href=""> <button type="button" class="btn btn-primary btn-lg btn-block">Đăng Nhập</button></a>
+
+                <div class="content-input-footer" >
+                    <div>Chưa có tài khoản</div>
+                    <div> <a href="./signUp.php"><u>Đăng Ký</u></a></div>
+                </div><br>
+
+            </form>
+
+        </div>
     </div>
-    <div class="footer">
-
+    <div class="footer" style="background-color :cadetblue;  height:125px;">
+        
+        <div class="footer-logo" style="padding-top:15px;">
+        
+                <p class="footer-logo-title" style="font-size: 50px;">FA</p>
+                <img class="logo" src="./resources/img/logo.jpg" alt="logo" style="height: 100px; width:100px;">
+                <p class="footer-logo-title" style="font-size: 50px;">TEAM</p>
+        </div>
     </div>
 </body>
+
+
+    <?php
+
+if(isset($_POST['btn']))
+{
+    if(empty($_POST['name']))
+    {
+        echo"Bạn chưa nhập vào tên đăng nhập";
+    }
+    else if(empty($_POST['password']))
+    {
+        echo "Bạn chưa nhập mật khẩu";
+    }
+    else
+    {
+        $username = $_POST['name'];
+        $pass = $_POST['password'];
+
+
+
+    }
+}
+?>
 <script src="./resources/js/login.js"></script>
 
 </html>
