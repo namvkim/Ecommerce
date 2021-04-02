acd9c1130cbc2dad6d15272918cec001ca4e94d9
namvkim <sonnamttt@gmail.com>
2021-04-02 19:11:51 +0700

    need check oop

--- resources/data/user.php
+++ resources/data/user.php
@@ -14,22 +14,22 @@
             $data= $conn->connect();
             $sql="INSERT INTO users( name_user, pass, phone_num, email, address) 
             VALUES('$name', '$password', $phone, '$email', '$address')";
-            return $conn->req($data,$sql);
+            $conn->req($data,$sql);
         }
 
         public function delete($id){
             $conn = new connect_data();
             $data= $conn->connect();
             $sql="DELETE FROM users WHERE ID_user = '$id'";
-            return $conn->req($data,$sql);
+            $conn->req($data,$sql);
         }
 
         public function put($id, $name, $password, $phone,$email, $address){
             $conn = new connect_data();
             $data= $conn->connect();
             $sql ="UPDATE users SET name_user =' $name', pass = '$password', phone_num = $phone,
-              email = '$email', address = '$address' WHERE ID_user=$id ";
-            return $conn->req($data,$sql);
+            email = '$email', address = '$address' WHERE ID_user=$id ";
+            $conn->req($data,$sql);
         }
 
     }
